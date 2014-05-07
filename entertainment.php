81a1b738c000383d1803ae17845dabcc57c2f27f
Jbouska419 <Jbouska419@gmail.com>
2014-05-07 17:05:19 -0500

    Create entertainment.php

---
+++ entertainment.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>Oakbrook Night Life</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 0.20" />
+	<link rel="stylesheet" type="text/css" href="includes/style.css">
+	<link rel="shortcut icon" href="images/favicon2.ico" >
+	<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
+	<script src="_js/jquery-1.6.3.min.js"></script>
+	<script src="_js/jquery.validate.min.js"></script>
+<script src="bxslider/jquery.bxSlider.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+  $(function(){
+  $('#js-slider').bxSlider({
+    auto: true,
+    autoControls: true,
+    mode: 'fade',
+    prevText: null,
+    nextText: null,
+    stopText: null,
+    speed: 3000,
+  });
+}); 
+	</script>
+<script type="text/javascript">
+  $(function(){
+  $('#ad_1').bxSlider({
+    auto: true,
+    autoControls: true,
+    prevText: null,
+    nextText: null,
+    stopText: null,
+    speed: 3000,
+  });
+}); 
+	</script>	
+<script type="text/javascript">
+  $(function(){
+  $('#ad_2').bxSlider({
+    auto: true,
+    autoControls: true,
+    prevText: null,
+    nextText: null,
+    stopText: null,
+    speed: 3000,
+  });
+}); 
+	</script>	
+</head>
+
+<body>
+	<div id="shell">
+		<header>
+	
+		</header>
+			<div id="main-content">
+				<section id="left-column">
+					<nav id="main">
+						<ul>
+							<li><a href="index2.php">Home</a></li>
+							<li><a href="drink_specials.php">Drink Specials</a></li>
+							<li><a href="dining_specials.php">Dining Specials</a></li>
+							<li><a href="events.php">Events</a></li>
+							<li><a href="live_music.php">Live Music</a></li>
+							<li><a href="entertainment.php">Entertainment</a></li>
+						</ul>
+					</nav>
+					<div id="ad_1">
+							<div><img src="images/slider/capital_grille_ad.jpg" alt="capital grille"></div>
+							<div><img src="images/slider/mccormick_schmicks_ad.png" alt="mccormick & schmicks"></div>
+							<div><img src="images/slider/tilted_kilt_ad.jpg" alt="tilted kilt"></div>
+							<div><img src="images/slider/luckystrike_ad.jpg" alt="lucky strike lanes"></div>
+						</div> 
+					<div class="cl"></div>
+					
+			
+				</section>
+				
+				<section id="center">
+					<div id="entertainment">
+						<h2 class="entertainment">Entertainment</h2>
+							<div id="drury_lane">
+								<img src="images/entertainment/drury_lane_logo.jpeg" alt="Drury Lane" />
+								<h3 class="yellow">Drury Lane</h3>
+									<p>August 15th - October 6th</p>
+									<div id="current_play">
+										<img src="images/entertainment/current_drury_play.jpeg" alt="Next to Normal" />
+									</div>
+									<a href="http://www.ticketmaster.com/search?tm_link=tm_header_search&user_input=Drury+Lane+Theatre%2C+Next+to+Normal&q=Drury+Lane+Theatre%2C+Next+to+Normal">Click here to purchase tickets</a>
+							</div>
+							<div id="drury_lane">
+									<img class="restaurant_logo" src="images/entertainment/amc_logo.jpeg" alt="AMC Theaters" />
+									<h3>AMC Theaters</h3>
+										<p>Now Showing</p>
+										<div id="current_play">
+											<img src="images/entertainment/Closed-Circuit-2013.jpg" alt="Closed Circuit" />
+										</div>
+										<a href="http://www.amctheatres.com/movie-theatres/amc-yorktown-17"><span>Click here to purchase tickets or to view over movies</span></a>
+								</div>
+								<div id="drury_lane">
+									<img class="restaurant_logo" src="images/entertainment/oakbrook_gc_logo.jpeg" alt="Oak Brook Golf Club" />
+									<h3>Oak Brook Golf Club</h3>
+										<p>Oak Brook Golf Course is a classic playing experience in a spectacular natural setting
+										 where every hole calls for solid shot-making from tee to green.  This is a thinking person's course that rewards
+										   sound play for players at any skill level.</p>
+										<div id="current_play">
+											<img src="images/entertainment/oakbrook_golf_pic.jpeg" alt="Oak Brook Golf Club" />
+										</div>
+										<a href="http://www.oak-brook.org/index.aspx?nid=234"><span>Make a tee time online or call (630) 683-5000</span></a>
+								</div>
+					</div>
+					
+				</section>
+			
+				<section id="right-column">
+						<h1>&nbsp;&nbsp;Follow Us on Facebook and Twitter</h1>
+					<div id="social">
+						<a href="http://www.facebook.com/oakbrooknightlife"><img src="images/facebook2.png" alt="facebook" /></a>
+						<a href="https://twitter.com/@OB_Nightlife"><img src="images/twitter2.png" alt="twitter" /></a>
+					</div>
+					<h1>Upcoming Events</h1>
+					<div id="upcoming_events">
+						<img src="images/events/bar_louie_logo.jpeg" alt="" /><a href="http://www.ufc.com/event/UFC165">UFC 165</a>
+							<p>Sat. Sept. 21st @ 8pm.</p>
+						<div class="cl"></div>
+						<img src="images/events/clubhouse_logo.jpeg" alt="" /><a href="http://www.theclubhouse.com/firstfridaysept">First Friday</a>
+							<p>Fri. September 6th starts @ 4pm</p>
+						<div class="cl"></div>
+						<img src="images/events/antico_posto_logo.jpeg" alt="" /><a href="#">Talbott Wine Dinner</a>
+							<p>Thurs. October 3rd </p>
+						<div class="cl"></div>
+						<img src="images/events/wildfire_logo.jpeg" alt="" /><a href="http://www.wildfirerestaurant.com/events/wine-events">Ferrari-Carano Wine Dinner</a>
+							<p>Mon. October 14th @ 6:30pm</p>
+						<div class="cl"></div>
+					</div>
+					<div id="advertise">
+						<img src="images/advertise_here.png" alt="advertise here" />
+					</div>
+					<div id="ad_2">
+							<div><img src="images/slider/wildfire_ad.jpeg" alt="wildfire"></div>
+							<div><img src="images/slider/houlihans_ad.jpeg" alt="houlihans"></div>
+							<div><img src="images/slider/trademark_ad.jpeg" alt="trademark tavern"></div>
+							<div><img src="images/slider/dao_ad.jpg" alt="dao sushi"></div>
+						</div>
+					<div class="cl"></div>
+					
+					
+				</section>
+			</div>
+	
+	</div>
+</body>
+

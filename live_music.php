db6fe6a6d1a1836d918fc2dd1645bb544a3e42aa
Jbouska419 <Jbouska419@gmail.com>
2014-05-07 17:07:00 -0500

    Create live_music.php

---
+++ live_music.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>Oakbrook Night Life</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 0.20" />
+	<link rel="stylesheet" type="text/css" href="includes/style.css">
+	<link rel="shortcut icon" href="images/favicon2.ico" >
+	<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
+	<script src="_js/jquery-1.6.3.min.js"></script>
+	<script src="_js/jquery.validate.min.js"></script>
+<script src="bxslider/jquery.bxSlider.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+  $(function(){
+  $('#js-slider').bxSlider({
+    auto: true,
+    autoControls: true,
+    mode: 'fade',
+    prevText: null,
+    nextText: null,
+    stopText: null,
+    speed: 3000,
+  });
+}); 
+	</script>
+<script type="text/javascript">
+  $(function(){
+  $('#ad_1').bxSlider({
+    auto: true,
+    autoControls: true,
+    prevText: null,
+    nextText: null,
+    stopText: null,
+    speed: 3000,
+  });
+}); 
+	</script>	
+<script type="text/javascript">
+  $(function(){
+  $('#ad_2').bxSlider({
+    auto: true,
+    autoControls: true,
+    prevText: null,
+    nextText: null,
+    stopText: null,
+    speed: 3000,
+  });
+}); 
+	</script>	
+</head>
+
+<body>
+	<div id="shell">
+		<header>
+	
+		</header>
+			<div id="main-content">
+				<section id="left-column">
+					<nav id="main">
+						<ul>
+							<li><a href="index2.php">Home</a></li>
+							<li><a href="drink_specials.php">Drink Specials</a></li>
+							<li><a href="dining_specials.php">Dining Specials</a></li>
+							<li><a href="events.php">Events</a></li>
+							<li><a href="live_music.php">Live Music</a></li>
+							<li><a href="entertainment.php">Entertainment</a></li>
+						</ul>
+					</nav>
+					<div id="ad_1">
+							<div><img src="images/slider/capital_grille_ad.jpg" alt="capital grille"></div>
+							<div><img src="images/slider/mccormick_schmicks_ad.png" alt="mccormick & schmicks"></div>
+							<div><img src="images/slider/tilted_kilt_ad.jpg" alt="tilted kilt"></div>
+							<div><img src="images/slider/luckystrike_ad.jpg" alt="lucky strike lanes"></div>
+						</div> 
+					<div class="cl"></div>
+					
+			
+				</section>
+				
+				<section id="center">
+					<div id="live_music">
+						<h2 class="entertainment">Live Music</h2>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/beer_house_logo.jpeg" alt="Beer House" />
+									<h3 class="yellow">Beer House</h3>
+									<h3><span>Jacob Duffy</span></h3>
+										<p><span>Thursday September, 19th @ 7pm</span></p>
+									<img class="artist" src="images/live_music/jacob_duffy.jpg" alt="Jacob Duffy" />
+								</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/beer_house_logo.jpeg" alt="Beer House" />
+									<h3 class="yellow">Beer House</h3>
+									<h3><span>Mighty Joe</span></h3>
+										<p><span>Friday September, 20th @ 7pm</span></p>
+									<img class="artist" src="images/live_music/mighty_joe.jpg" alt="Mighty Joe" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/pinstripes_logo.jpeg" alt="Pinstripes" />
+									<h3 class="yellow">Pinstripes</h3>
+									<h3><span>Skinny Williams</span></h3>
+										<p><span>Friday September, 20th @ 7:30pm</span></p>
+									<img class="artist" src="images/live_music/skinny_williams.jpeg" alt="Skinny Williams" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/trademark_logo.png" alt="Trademark Tavern" />
+									<h3 class="yellow">Trademark Tavern</h3>
+									<h3><span>1969 Band</span></h3>
+										<p><span>Friday September, 20th @ 9:30pm</span></p>
+									<img class="artist" src="images/live_music/1969_band.jpg" alt="1969 Band" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/grotto_logo.jpg" alt="Grotto" />
+									<h3 class="yellow">Grotto</h3>
+									<h3><span>Jimmy L.</span></h3>
+										<p><span>Saturday September, 21st @ 8:30pm</span></p>
+									<img class="artist" src="images/live_music/no_image.jpg" alt="No Image Available" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/pinstripes_logo.jpeg" alt="Pinstripes" />
+									<h3 class="yellow">Pinstripes</h3>
+									<h3><span>Ray Silkman</span></h3>
+										<p><span>Saturday September, 21st @ 7:30pm</span></p>
+									<img class="artist" src="images/live_music/ray_silkman.jpeg" alt="Ray Silkman" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/trademark_logo.png" alt="Trademark Tavern" />
+									<h3 class="yellow">Trademark Tavern</h3>
+									<h3><span>The Mix</span></h3>
+										<p><span>Saturday September, 21st @ 9pm</span></p>
+									<img class="artist" src="images/live_music/the_mix.jpeg" alt="The Mix" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/beer_house_logo.jpeg" alt="Beer House" />
+									<h3 class="yellow">Beer House</h3>
+									<h3><span>Keith Semple</span></h3>
+										<p><span>Thursday September, 26th @ 7pm</span></p>
+									<img class="artist" src="images/live_music/keith_semple.jpg" alt="Keith Semple" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/beer_house_logo.jpeg" alt="Beer House" />
+									<h3 class="yellow">Beer House</h3>
+									<h3><span>Simple Remedy</span></h3>
+										<p><span>Friday September, 27th @ 7pm</span></p>
+									<img class="artist" src="images/live_music/simple_remedy.png" alt="Simple Remedy" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/pinstripes_logo.jpeg" alt="Pinstripes" />
+									<h3 class="yellow">Pinstripes</h3>
+									<h3><span>Ray Silkman</span></h3>
+										<p><span>Friday September, 27th @ 7:30pm</span></p>
+									<img class="artist" src="images/live_music/ray_silkman.jpeg" alt="Ray Silkman" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/trademark_logo.png" alt="Trademark Tavern" />
+									<h3 class="yellow">Trademark Tavern</h3>
+									<h3><span>R-Gang</span></h3>
+										<p><span>Friday September, 27th @ 9pm</span></p>
+									<img class="artist" src="images/live_music/r-gang.jpeg" alt="R-Gang" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/grotto_logo.jpg" alt="Grotto" />
+									<h3 class="yellow">Grotto</h3>
+									<h3><span>Mike Hanko</span></h3>
+										<p><span>Saturday September, 28th @ 8:30pm</span></p>
+									<img class="artist" src="images/live_music/no_image.jpg" alt="No Image Available" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/pinstripes_logo.jpeg" alt="Pinstripes" />
+									<h3 class="yellow">Pinstripes</h3>
+									<h3><span>Audley Reid</span></h3>
+										<p><span>Saturday September, 28th @ 7:30pm</span></p>
+									<img class="artist" src="images/live_music/audley_reid.jpeg" alt="Audley Reid" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/beer_house_logo.jpeg" alt="Beer House" />
+									<h3 class="yellow">Beer House</h3>
+									<h3><span>Windy City Rev Ups</span></h3>
+										<p><span>Saturday October, 5th @ 7pm</span></p>
+									<img class="artist" src="images/live_music/windy_city_rev_ups.jpg" alt="Windy City Rev Ups" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/beer_house_logo.jpeg" alt="Beer House" />
+									<h3 class="yellow">Beer House</h3>
+									<h3><span>Vanessa Antonino</span></h3>
+										<p><span>Friday October, 11th @ 8pm</span></p>
+									<img class="artist" src="images/live_music/vanessa_antonino.jpg" alt="Vanessa Antonino" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/beer_house_logo.jpeg" alt="Beer House" />
+									<h3 class="yellow">Beer House</h3>
+									<h3><span>Chris Corsale</span></h3>
+										<p><span>Friday October, 18th @ 9pm</span></p>
+									<img class="artist" src="images/live_music/chris_corsale.jpg" alt="Chris Corsale" />
+							</div>
+							<div id="restaurant">
+									<img class="restaurant_logo" src="images/live_music/beer_house_logo.jpeg" alt="Beer House" />
+									<h3 class="yellow">Beer House</h3>
+									<h3><span>Mighty Joe</span></h3>
+										<p><span>Friday October, 25th @ 8pm</span></p>
+									<img class="artist" src="images/live_music/mighty_joe.jpg" alt="Mighty Joe" />
+							</div>
+					</div>
+				</section>
+			
+				<section id="right-column">
+						<h1>&nbsp;&nbsp;Follow Us on Facebook and Twitter</h1>
+					<div id="social">
+						<a href="http://www.facebook.com/oakbrooknightlife"><img src="images/facebook2.png" alt="facebook" /></a>
+						<a href="https://twitter.com/@OB_Nightlife"><img src="images/twitter2.png" alt="twitter" /></a>
+					</div>
+					<h1>Upcoming Events</h1>
+					<div id="upcoming_events">
+						<img src="images/events/bar_louie_logo.jpeg" alt="" /><a href="http://www.ufc.com/event/UFC165">UFC 165</a>
+							<p>Sat. Sept. 21st @ 8pm.</p>
+						<div class="cl"></div>
+						<img src="images/events/clubhouse_logo.jpeg" alt="" /><a href="http://www.theclubhouse.com/firstfridaysept">First Friday</a>
+							<p>Fri. September 6th starts @ 4pm</p>
+						<div class="cl"></div>
+						<img src="images/events/antico_posto_logo.jpeg" alt="" /><a href="#">Talbott Wine Dinner</a>
+							<p>Thurs. October 3rd </p>
+						<div class="cl"></div>
+						<img src="images/events/wildfire_logo.jpeg" alt="" /><a href="http://www.wildfirerestaurant.com/events/wine-events">Ferrari-Carano Wine Dinner</a>
+							<p>Mon. October 14th @ 6:30pm</p>
+						<div class="cl"></div>
+					</div>
+					<div id="advertise">
+						<img src="images/advertise_here.png" alt="advertise here" />
+					</div>
+					<div id="ad_2">
+							<div><img src="images/slider/wildfire_ad.jpeg" alt="wildfire"></div>
+							<div><img src="images/slider/houlihans_ad.jpeg" alt="houlihans"></div>
+							<div><img src="images/slider/trademark_ad.jpeg" alt="trademark tavern"></div>
+							<div><img src="images/slider/dao_ad.jpg" alt="dao sushi"></div>
+						</div>
+					<div class="cl"></div>
+					
+					
+				</section>
+			</div>
+	
+	</div>
+</body>
+
